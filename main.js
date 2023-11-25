6d352b772ac8aec1aaea4df8e05b948e1cb8c394
EUIJOON <euijoonism@gmail.com>
2023-11-26 02:27:37 +0900

    [feat][refactor] 각 결과 시 보여줄 gif 파일 추가, 변수명, 출력 메시지 등 수정

--- main.js
+++ main.js
@@ -3,19 +3,23 @@ const MIN_NUMBER = 1;
 const MAX_ATTEMPTS = 10;
 
 let comNumber = 0;
-let startButton = document.getElementById("start-button"); // 웹사이트 전체에서 요소를 id로 가져와서 집어넣겠다.
-let inputField = document.getElementById("user-input-field");
-let gameResultDisplay = document.getElementById("result-area");
-let resetGameButton = document.getElementById("game-reset-button");
+let startButton = document.getElementById("start-button");
+let gameResetButton = document.querySelector(".game-reset-button");
+let inputField = document.querySelector("#user-input-field")
+let gameResultAreaImage = document.querySelector(".main-image");
+let gameResultDisplay = document.querySelector(".result-text");
 let opportunityArea = document.getElementById("opportunity-display");
+
+
 
 let opportunity = MAX_ATTEMPTS;
 let isGameOver = false;
 let userGuessHistory = []; // 유저가 입력한 숫자 저장해둘 배열
 
+opportunityArea.innerHTML = `남은 기회: ${opportunity}`
 inputField.addEventListener("focus", clearUserInputField);
 startButton.addEventListener("click", playGame); 
-resetGameButton.addEventListener("click", resetGame);
+gameResetButton.addEventListener("click", resetGame);
 
 
 function clearUserInputField() {
@@ -47,23 +51,26 @@ function playGame() {
     }
 
     opportunity--;
-    opportunityArea.textContent = `남은 기회 : ${opportunity} 번`;
-
-    if(userGuess > comNumber) {
-        gameResultDisplay.textContent = "DOWN!";
-    } else if(userGuess < comNumber) {
-        gameResultDisplay.textContent = "UP!";
-    } else {
-        gameResultDisplay.textContent = "정답입니다!";
-        isGameOver = true;
-    }
-
+    opportunityArea.innerHTML = `남은 기회: ${opportunity}`;
     // 유저가 입력한 기록
     userGuessHistory.push(userGuess); // 이 배열에 유저가 입력한 기록을 다 저장
     console.log(userGuessHistory);
 
+    // 결과 검증
+    if(userGuess > comNumber) {
+        gameResultAreaImage.src = "image/down.gif";
+        gameResultDisplay.textContent = "DOWN!";
+    } else if(userGuess < comNumber) {
+        gameResultAreaImage.src = "image/up.gif";
+        gameResultDisplay.textContent = "UP!";
+    } else {
+        gameResultAreaImage.src = "image/correct.gif";
+        gameResultDisplay.textContent = "정답입니다!";
+        isGameOver = true;
+    }
 
-    if(opportunity < 1) {
+
+    if(opportunity == 0) {
         isGameOver = true;
 
         // 남은 기회가 0일 때 정답 알려주기
@@ -88,16 +95,18 @@ function resetGame() {
     inputField.value = "";
     // 새 번호가 생성되고
     generateRandomNumber();
+
+    gameResultAreaImage.src = "image/guess.gif"; 
     // 멘트도 초기화
-    gameResultDisplay.textContent = "결과는~!"
+    gameResultDisplay.textContent = "추측해보시라~";
+    isGameOver = false;
 
     // 남은 기회 초기화
     opportunity = MAX_ATTEMPTS;
-    opportunityArea.textContent = `남은 기회 : ${opportunity} 번`;
+    opportunityArea.innerHTML = `남은 기회: ${opportunity}`;
         
     // 유저가 입력한 기록 초기화
     userGuessHistory = [];
-
     // Start 버튼 다시 활성화
     startButton.disabled = false;
 }
